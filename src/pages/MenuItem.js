f7b437a20aa04d7566172f7b9546442b5b5b1c4e
Kostas Ntallas <kostasdallas2010@hotmail.com>
2019-11-04 23:22:23 +0200

    implemented hover effect for menu items

--- src/pages/MenuItem.js
+++ src/pages/MenuItem.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 const MenuItem = ({title, imageUrl, size}) => {
   return (
-    <div 
-      className={`menu-item ${size}`}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
+    <div className={`menu-item ${size}`}>
+      <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
