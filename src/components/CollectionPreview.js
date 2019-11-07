8539f1e7305dd99cc2fa1bfa17535c98a63e987d
Kostas Ntallas <kostasdallas2010@hotmail.com>
2019-11-07 20:57:55 +0200

    implemeted collection item component

--- src/components/CollectionPreview.js
+++ src/components/CollectionPreview.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import CollectionItem from './CollectionItem';
 
 const CollectionPreview = ({title, items}) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.filter((item, index) => index < 4).map((item) => (
-        <div key={item.id}>{item.name}</div>
+      {items.filter((item, index) => index < 4).map(({id, ...otherItemProps}) => (
+        <CollectionItem key={id} {...otherItemProps}/>
       ))}
     </div>
   </div>
