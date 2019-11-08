729da1170d02cdd965e2507240719f67fe84830a
Kostas Ntallas <kostasdallas2010@hotmail.com>
2019-11-08 21:27:08 +0200

    implemented basic sign in

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './Header';
 import HomePage from '../pages/HomePage';
 import ShopPage from '../pages/ShopPage';
+import SignPage from '../pages/SignPage';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/SignPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SignIn from '../components/SignIn';
+
+const SignPage = () => (
+  <div className="sign-in-sign-out">
+    <SignIn />
+  </div>
+)
+
+export default SignPage;
