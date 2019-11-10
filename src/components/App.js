6d5e7a076c52f4a458da0e03acd281ce9615b847
Kostas Ntallas <kostasdallas2010@hotmail.com>
2019-11-10 13:21:17 +0200

    storing data in firebase and app

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Header from './Header';
 import HomePage from '../pages/HomePage';
 import ShopPage from '../pages/ShopPage';
 import SignPage from '../pages/SignPage';
-import { auth } from '../firebase/firebase.utils';
+import { auth, createUserProfileDocument } from '../firebase/firebase.utils';
 
 class App extends Component {
   state = {
@@ -14,8 +14,23 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      } else {
+        this.setState({
+          currentUser: userAuth
+        });
+      }
     });
   }
 
