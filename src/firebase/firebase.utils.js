e79c959f9c0a72a23d7188a7a3ebdb7f01267f60
Kostas Ntallas <kostasdallas2010@hotmail.com>
2020-01-09 23:55:17 +0200

    bringing shop data to app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -43,7 +43,6 @@ firebase.initializeApp(config);
 
 export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
   const collectionRef = firestore.collection(collectionKey);
-
   const batch = firestore.batch();
   objectsToAdd.forEach(obj => {
     const newDocRef = collectionRef.doc();
@@ -51,6 +50,21 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   });
 
   return await batch.commit();
+}
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+
+  console.log(transformedCollection);
 }
 
 export const auth = firebase.auth();
